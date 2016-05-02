725ee079004f52bfd92b027dc3fccbb971bc46c7
michael.shaw <michael.and.shaw@gmail.com>
2016-05-02 22:18:59 +0100

    Reverted to simple example

--- flask_microbrewery/app/models.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-class Beer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Unicode, unique=True)
-
-
--- flask_microbrewery/run.py
+++ flask_microbrewery/run.py
@@ -1,13 +1,49 @@
+from flask import Flask
 from flask.ext.restless import APIManager
-from flask_microbrewery.app.models import Beer
-from app import app, db
+from flask.ext.sqlalchemy import SQLAlchemy
+
+# Create the flash App and attach a SQLAlchemy db from the DB spec
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+# Define the models
+class Beer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode, unique=True)
+    type_id = db.Column(db.Integer, db.ForeignKey('type.id'))
+    reviews = db.relationship('Review', backref=db.backref('beer'), lazy='dynamic')
+
+class Type(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode, unique=True)
+    beers = db.relationship('Beer', backref=db.backref('type'), lazy='dynamic')
+
+class Reviewer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode, unique=True)
+    reviews = db.relationship('Review', backref=db.backref('author'), lazy='dynamic')
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime)
+    stars = db.Column(db.Float)
+    comment = db.Column(db.Unicode)
+    beer_id = db.Column(db.Integer, db.ForeignKey('beer.id'))
+    author_id = db.Column(db.Integer, db.ForeignKey('reviewer.id'))
+
+# Create the DB
+db.create_all()
 
 # Create the Flask-Restless API manager.
 manager = APIManager(app, flask_sqlalchemy_db=db)
 
-# Create API endpoints, which will be available at /api/<tablename> by
-# default. Allowed HTTP methods can be specified as well.
-manager.create_api(Beer, methods=['GET', 'POST', 'DELETE'])
+# Create the API endpoints from the DB Models
+# These will be available at /api/<tablename>
+manager.create_api(Beer, methods=['GET', 'POST', 'PUT', 'DELETE'])
+manager.create_api(Type, methods=['GET', 'POST', 'PUT', 'DELETE'])
+manager.create_api(Review, methods=['GET', 'POST', 'PUT', 'DELETE'])
+manager.create_api(Reviewer, methods=['GET', 'POST', 'PUT', 'DELETE'])
 
 # start the flask app
 app.run(debug=True)
